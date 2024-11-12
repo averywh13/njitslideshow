b1bd9b10e38f6f5e9aad226695486181fc0ad16e
averywh13 <awekslerhart@gmail.com>
2024-11-12 11:48:55 -0500

    fixed background

--- gallery.js
+++ gallery.js
@@ -3,6 +3,11 @@ let mImages = [] // Array to hold GalleryImage objects
 const mUrl = 'images.json' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
+
+
+
+
+
 $(document).ready(() => {
   $('.details').hide() // Hide details initially 
 
@@ -29,6 +34,12 @@ $(document).ready(() => {
   fetchJSON()
 })
 
+
+
+
+
+
+
 // Function to fetch JSON data and store it in mImages
 function fetchJSON() {
   // Use $.ajax here to request the JSON data from mUrl
@@ -52,12 +63,24 @@ function fetchJSON() {
   });
 }
 
+
+
+
+
+
+
 // Function to swap and display the next photo in the slideshow
 function swapPhoto() {
   // Access mImages[mCurrentIndex] to update the image source and details
   // Update the #photo element's src attribute with the current image's path
   // Update the .location, .description, and .date elements with the current image's details
 }
+
+
+
+
+
+
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
 function showNextPhoto() {
@@ -71,6 +94,11 @@ function showNextPhoto() {
   console.log(mCurrentIndex);
 }
 
+
+
+
+
+
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto() {
   // Decrement mCurrentIndex and call swapPhoto()
@@ -82,6 +110,12 @@ function showPrevPhoto() {
   }
   console.log(mCurrentIndex);
 }
+
+
+
+
+
+
 
 // Starter code for the timer function
 function startTimer() {
